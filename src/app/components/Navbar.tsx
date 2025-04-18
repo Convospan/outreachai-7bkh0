1dd99062b34dda874023bc8e479ef957e2e494cf
Siddharth Tewari <tewari.85@gmail.com>
2025-04-18 12:35:51 +0000

    no make hotizontal menu of all modules and theri chaild as drop down

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -18,34 +18,54 @@ const Navbar = () => {
       <div className="container flex items-center justify-between py-2">
         <Link href="/" className="font-bold text-xl">ConvoSpan.ai</Link>
         <div className="flex items-center space-x-4">
-          <Link href="/" className="hover:underline">Home</Link>
-          <Link href="/about" className="hover:underline">About Us</Link>
+          <Link href="/" >Home</Link>
+          <Link href="/about" >About Us</Link>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="ghost">
-                Modules <Icons.chevronDown className="h-4 w-4 ml-1" />
+                Campaign <Icons.chevronDown className="h-4 w-4 ml-1" />
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56">
-              <DropdownMenuLabel>Outreach</DropdownMenuLabel>
               <DropdownMenuItem asChild>
                 <Link href="/campaign/create">Campaign Creation</Link>
               </DropdownMenuItem>
               <DropdownMenuItem asChild>
                   <Link href="/campaign">Campaign Automation</Link>
               </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuLabel>Compliance</DropdownMenuLabel>
+            </DropdownMenuContent>
+          </DropdownMenu>
+             <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost">
+                Compliance <Icons.chevronDown className="h-4 w-4 ml-1" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
               <DropdownMenuItem asChild>
                 <Link href="/compliance/check">Compliance Check</Link>
               </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuLabel>Calling</DropdownMenuLabel>
+            </DropdownMenuContent>
+          </DropdownMenu>
+             <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost">
+                Calling <Icons.chevronDown className="h-4 w-4 ml-1" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
               <DropdownMenuItem asChild>
                 <Link href="/call/approve">Call Script Approval</Link>
               </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuLabel>Visualization</DropdownMenuLabel>
+            </DropdownMenuContent>
+          </DropdownMenu>
+                   <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost">
+                Visualization <Icons.chevronDown className="h-4 w-4 ml-1" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
               <DropdownMenuItem asChild>
                 <Link href="/risk-lead-visualization">Risk & Lead Visualization</Link>
               </DropdownMenuItem>
